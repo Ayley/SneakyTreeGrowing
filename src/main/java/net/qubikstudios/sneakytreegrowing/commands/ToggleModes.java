111cf4be90e051661cf75bafc0890242b5105ef8
Ayley <larshatzky@gmail.com>
2022-10-06 21:18:56 +0200

    Update to the new config system

--- src/main/java/net/qubikstudios/sneakytreegrowing/commands/ToggleModes.java
+++ src/main/java/net/qubikstudios/sneakytreegrowing/commands/ToggleModes.java
@@ -1,25 +1,23 @@
 package net.qubikstudios.sneakytreegrowing.commands;
 
 import net.qubikstudios.sneakytreegrowing.SneakyTreeGrowing;
-import org.bukkit.Bukkit;
+import net.qubikstudios.sneakytreegrowing.util.ConfigNames;
+import net.qubikstudios.sneakytreegrowing.util.PluginConfig;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
-import org.bukkit.configuration.file.FileConfiguration;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class ToggleModes implements TabExecutor {
 
     private final SneakyTreeGrowing instance;
-    private final FileConfiguration conf;
+    private final PluginConfig conf;
 
     public ToggleModes(SneakyTreeGrowing instance) {
         this.instance = instance;
-        this.conf = instance.getConfig();
+        this.conf = instance.getPluginConfig();
     }
 
     @Override
@@ -30,23 +28,23 @@ public class ToggleModes implements TabExecutor {
 
         if(args[0].equals("toggle")){
             if(args[1].equals("tree")){
-                var currentState = conf.getBoolean("SneakyTreeGrowing.TreeSettings.EnableTreeMeal");
+                var currentState = conf.getBoolean(ConfigNames.TREE_ENABLED);
                 var result = currentState ? "§cdisabled" : "§aenabled";
-                conf.set("SneakyTreeGrowing.TreeSettings.EnableTreeMeal", !currentState);
+                conf.set(ConfigNames.TREE_ENABLED, !currentState);
                 sender.sendMessage("Growing trees are now set to: " + result);
             }
 
             if(args[1].equals("crops")){
-                var currentState = conf.getBoolean("SneakyTreeGrowing.CropSettings.EnableCropMeal");
+                var currentState = conf.getBoolean(ConfigNames.CROP_ENABLED);
                 var result = currentState ? "§cdisabled" : "§aenabled";
-                conf.set("SneakyTreeGrowing.CropSettings.EnableCropMeal", !currentState);
+                conf.set(ConfigNames.CROP_ENABLED, !currentState);
                 sender.sendMessage("Growing crops are now set to: " + result);
             }
 
             if(args[1].equals("tags")){
-                var currentState = conf.getBoolean("SneakyTreeGrowing.CustomTag.EnableCustomTags");
+                var currentState = conf.getBoolean(ConfigNames.CUSTOM_ENABLED);
                 var result = currentState ? "§cdisabled" : "§aenabled";
-                conf.set("SneakyTreeGrowing.CustomTag.EnableCustomTags", !currentState);
+                conf.set(ConfigNames.CUSTOM_ENABLED, !currentState);
                 sender.sendMessage("Growing tags are now set to: " + result);
             }
             instance.saveConfig();
